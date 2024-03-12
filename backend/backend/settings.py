4fa2766762a13e6c0ed5834869672ee6627bf140
Arun-cb <106576871+Arun-cb@users.noreply.github.com>
2024-03-12 09:35:00 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -244,7 +244,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 # MEDIA_ROOT = os.path.join(BASE_DIR, CLIENT_MEDIA_PATH)
 
 # Store image in server
-SERVER_MEDIA_PATH = './home/ubuntu/images'
+SERVER_MEDIA_PATH = 'home/ubuntu/images'
 MEDIA_ROOT = os.path.join(BASE_DIR, SERVER_MEDIA_PATH)
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'C:/Arun/POC/scorecard_build_folder/build/Assets/master')
