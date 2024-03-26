90adc8c866e38770e947f03eb63c06b595329fc8
Arun-cb <106576871+Arun-cb@users.noreply.github.com>
2024-03-26 15:25:19 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -269,11 +269,12 @@ CORS_ALLOW_ALL_ORIGINS = True
 # MEDIA_ROOT = os.path.join(BASE_DIR, CLIENT_MEDIA_PATH)
 
 # Store image in server
+# SERVER_MEDIA_PATH = '/home/ubuntu/images' 
 # SERVER_MEDIA_PATH = '../../../../var/www/html/my-react-app/build/Assets/master'
 # MEDIA_ROOT = os.path.join(BASE_DIR, SERVER_MEDIA_PATH)
 
 # SERVER_MEDIA_PATH = 'http://13.201.143.57/Assets/master'
-SERVER_MEDIA_PATH = '/home/ubuntu/images'
+SERVER_MEDIA_PATH = '/var/www/html/my-react-app/build/Assets/master'
 MEDIA_ROOT = os.path.join(BASE_DIR, SERVER_MEDIA_PATH)
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'C:/Arun/POC/scorecard_build_folder/build/Assets/master')
